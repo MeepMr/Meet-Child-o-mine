a15b3fdb590ba0015f7bfeb9154828fbd1c05d62
justinklein <jus.klein@icloud.com>
2021-01-28 12:36:53 +0100

    First Sample-Bot finished

---
+++ firstclient.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./configuration.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+console.log(config.token);
+client.login(config.token);
