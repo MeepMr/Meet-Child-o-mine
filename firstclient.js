9d4db49dc1ab5d58cba3e5eb189fab651c5c972e
justinklein <jus.klein@icloud.com>
2021-01-28 12:49:16 +0100

    First Sample-Bot finished

--- firstclient.js
+++ firstclient.js
@@ -4,6 +4,9 @@ const config = require('./configuration.json');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    let channel = client.channels.fetch("804308576184565760");
+    let ms = channel.messages;
+    console.log(ms);
 });
 
 client.on('message', msg => {
@@ -12,5 +15,7 @@ client.on('message', msg => {
     }
 });
 
-console.log(config.token);
-client.login(config.token);
+client.login(config.token).then((test) => {
+
+    console.log(`Logged in using Bot-Token ${test.substring(0,10)}...`);
+});
